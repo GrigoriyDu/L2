67a4dfe0252a25012e0d3e700a378d433bc12d4e
Grigorii Dukach <dukach055@yandex.ru>
2024-12-25 16:27:50 +0300

    Частичная релизация метода computeExpression и полная реализация метода isNumeric в классе MathExpressionCalculator

--- src/main/java/org/example/MathExpressionCalculator.java
+++ src/main/java/org/example/MathExpressionCalculator.java
@@ -1,8 +1,43 @@
 package org.example;
 
+import java.util.List;
+import java.util.Stack;
+
 /**
  * Класс для вычисления математических выражений.
  */
 public class MathExpressionCalculator {
 
+    /**
+     * Метод для вычисления математического выражения на основе алгоритма сортировочной станции.
+     *
+     * @param tokens математическое выражение
+     * @return результат вычисления выражения
+     */
+    public double computeExpression(List<String> tokens) {
+        if (tokens == null || tokens.isEmpty()) {
+            return 0;
+        }
+
+        Stack<Double> valueStack = new Stack<>();
+        Stack<String> operatorStack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isNumeric(token)){
+                // Пустой блок кода
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Метод для проверки, является ли строка числом.
+     *
+     * @param token строка для проверки
+     * @return true, если строка является числом, иначе false
+     */
+    private boolean isNumeric(String token) {
+        return Character.isDigit(token.charAt(0));
+    }
 }
+
--- src/main/java/org/example/MathExpressionParser.java
+++ src/main/java/org/example/MathExpressionParser.java
@@ -1,9 +1,77 @@
 package org.example;
 
+import java.util.List;
 
 /**
  * Класс для разбора математических выражений.
  */
 public class MathExpressionParser {
+    private String expression;
 
+    /**
+     * Конструктор класса.
+     *
+     * @param expression математическое выражение
+     */
+    public MathExpressionParser(String expression) {
+        this.expression = expression;
+    }
+
+    /**
+     * Получить математическое выражение.
+     *
+     * @return математическое выражение
+     */
+    public String getExpression() {
+        return expression;
+    }
+
+    /**
+     * Метод для проверки корректности математического выражения и вычисления его, если все в порядке.
+     *
+     * @return результат вычисления выражения
+     * @throws RuntimeException если скобки в выражении не сбалансированы
+     */
+    public double evaluateExpression() {
+        return 0;
+    }
+
+    /**
+     * Проверить сбалансированность скобок в математическом выражении.
+     *
+     * @return true, если скобки сбалансированы, иначе false
+     */
+    public boolean areBracketsBalanced() {
+        return false;
+    }
+
+    /**
+     * Разбор математического выражения на токены.
+     *
+     * @param expression математическое выражение
+     * @return список токенов
+     */
+    public List<String> parseExpression(String expression) {
+        return null;
+    }
+
+    /**
+     * Проверить, является ли символ оператором.
+     *
+     * @param c символ
+     * @return true, если символ является оператором, иначе false
+     */
+    public boolean isOperator(char c) {
+        return false;
+    }
+
+    /**
+     * Проверить, является ли символ скобкой.
+     *
+     * @param c символ
+     * @return true, если символ является скобкой, иначе false
+     */
+    public boolean isParenthesis(char c) {
+        return false;
+    }
 }
